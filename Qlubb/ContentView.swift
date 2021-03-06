e8972931fdc8676fd452dd302c44cd0936059935
Ethan <test@example.com>
2021-03-05 16:39:23 -0800

    Initial Commit

---
+++ Qlubb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Qlubb
+//
+//  Created by Ethan Hanlon on 3/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Qlubb/QlubbApp.swift
@@ -0,0 +1,17 @@
+//
+//  QlubbApp.swift
+//  Qlubb
+//
+//  Created by Ethan Hanlon on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct QlubbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
